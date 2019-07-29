2ee409966166cc2d0e35f5ac7fe26df663077672
Lenus Walker <walker.lenus@gmail.com>
2019-07-29 16:05:28 -0400

    Building out directories and initializing menu

--- main.js
+++ main.js
@@ -1,11 +1,9 @@
-const electron = require('electron');
+const {app, BrowserWindow, Menu} = require('electron');
 const url = require('url');
 const path = require('path');
 
-const { app, BrowserWindow} = electron;
-
-let loginWindow;
 let mainWindow;
+let loginWindow;
 
 //Listen for app to be ready
 app.on('ready', function(){
@@ -13,8 +11,30 @@ app.on('ready', function(){
     mainWindow = new BrowserWindow({});
     //Load html into window
     mainWindow.loadURL(url.format({
-        pathname: path.join(__dirname, 'mainWindow.html'),
+        pathname: path.join(__dirname, 'src/windows/mainWindow.html'),
         protocol: 'file:',
         slashes: true
     }));
+
+    //Build menu from template
+    const mainMenu = Menu.buildFromTemplate(mainMenuTemplate);
+    //Insert menu
+    Menu.setApplicationMenu(mainMenu);
 });
+
+//Create menu template
+const mainMenuTemplate = [
+    {
+        label: 'File',
+        submenu: [
+            {
+                label: 'Quit',
+                accelerator: process.platform == 'darwin' ? 'Command+Q' :
+                'Ctrl+Q',
+                click(){
+                    app.quit();
+                }
+            }
+        ]
+    }
+];
