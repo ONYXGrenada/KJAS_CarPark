f1ce9ba814f8c52130394c898b8b6c0b8271c0fa
Ozimba SubZero <ozimbat@hotmail.com>
2019-09-17 22:57:06 -0400

    Crude mock up of finding a ticket and displaying the information.

--- app/js/payTicketSub.js
+++ app/js/payTicketSub.js
@@ -1,30 +1,62 @@
 const {ipcRenderer} = require('electron')
-const dbhelper = require('../../app/js/dbhelper')
+//const dbhelper = require('../../app/js/dbhelper')
+const connection = require('../../app/js/dbconnection')
 
 
 //Listen for Ticket Number
-document.querySelector('#txtTicketNumber').addEventListener('change', async (e) => {
+document.querySelector('#txtTicketNumber').addEventListener('keyup', async(e) => {
     e.preventDefault()
-    console.log("ticket number field listener")
+    console.log("e.keycode " + e.keyCode)
     if (e.keyCode == 13) {
         const ticketNumber = document.querySelector('#txtTicketNumber').value
-        let ticket = await dbhelper.getTicket(ticketNumber)
+        let ticket = await connection.getTicket(ticketNumber)
+
+        //Set ticket values
+        let endTime = new Date()
+        let ticketDuration = convertMS(endTime - ticket.createdDate)
+        let ticketCost = ticket.rate * ticketDuration.hour
+        let displaySTime = ticket.createdDate.getFullYear() + "/" + ticket.createdDate.getMonth() + "/" + ticket.createdDate.getDate() + " " + ticket.createdDate.getHours() + ":" + ticket.createdDate.getMinutes() + ":" + ticket.createdDate.getSeconds()
+        let displayETime = endTime.getFullYear() + "/" + endTime.getMonth() + "/" + endTime.getDate() + " " + endTime.getHours() + ":" + endTime.getMinutes() + ":" + endTime.getSeconds()
+        document.getElementById('sTime').innerHTML = "Enter Time: " + displaySTime
+        document.getElementById('eTime').innerHTML = "Exit Time: " + displayETime
+        document.getElementById('duration').innerHTML = "Duration: " + ticketDuration.day + " day(s) " + ticketDuration.hour + " hour(s) " + ticketDuration.minute + " mintue(s) " + ticketDuration.seconds + " second(s)" 
+        document.getElementById('tType').innerHTML = "Ticket Type: " + ticket.ticketType
+        document.getElementById('tCost').innerHTML = "Cost: " + ticketCost
+
         //calculate cost (need ticket create date, rate and now)
-        console.log(ticket + " this is where the ticket is supposed to print")
+        console.log(ticket.id + " this is where the ticket is supposed to print")
     }
 })
 
+
+function convertMS( milliseconds ) {
+    var day, hour, minute, seconds;
+    seconds = Math.floor(milliseconds / 1000);
+    minute = Math.floor(seconds / 60);
+    seconds = seconds % 60;
+    hour = Math.floor(minute / 60);
+    minute = minute % 60;
+    day = Math.floor(hour / 24);
+    hour = hour % 24;
+    return {
+        day: day,
+        hour: hour,
+        minute: minute,
+        seconds: seconds
+    };
+}
+
 //Listen for Ticket Number TEST
-document.querySelector('#btnPayTicket').addEventListener('click', async (e) => {
-    e.preventDefault()
-    console.log("Pay Ticket Button listener")
+// document.querySelector('#btnPayTicket').addEventListener('click', async (e) => {
+//     e.preventDefault()
+//     console.log("Pay Ticket Button listener")
 
-    const ticketNumber = document.querySelector('#txtTicketNumber').value
-    let ticket = await dbhelper.getTicket(ticketNumber)
-    //calculate cost (need ticket create date, rate and now)
-    console.log(ticket + " this is where the ticket is supposed to print")
+//     const ticketNumber = document.querySelector('#txtTicketNumber').value
+//     let ticket = await dbhelper.getTicket(ticketNumber)
+//     //calculate cost (need ticket create date, rate and now)
+//     console.log(ticket + " this is where the ticket is supposed to print")
 
-})
+// })
 
 
 //listen for Pay Ticket button click
