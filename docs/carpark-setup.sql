c1e3480bae5c99a32c4acceb29db89e2b64873cb
Lenus Walker <lwalker@ariza-it-a.arizacu.com>
2019-11-02 11:13:37 -0400

    Updated carpark-setup.sql to fix datatypes and include more fields.

--- docs/carpark-setup.sql
+++ docs/carpark-setup.sql
@@ -9,69 +9,71 @@ USE carpark;
 -- Create tables
 CREATE TABLE IF NOT EXISTS `users` ( 
     `id` INTEGER UNSIGNED NOT NULL AUTO_INCREMENT, 
-    `username` TEXT NOT NULL, 
-    `password` TEXT, 
-    `salt` TEXT, 
-    `firstName` TEXT, 
-    `lastname` TEXT, 
-    `userType` TEXT, 
-    `lastLogin` TEXT, 
+    `username` VARCHAR(25) NOT NULL, 
+    `password` VARCHAR(255), 
+    `salt` VARCHAR(25), 
+    `firstName` VARCHAR(25), 
+    `lastname` VARCHAR(25), 
+    `userType` VARCHAR(25), 
+    `lastLogin` DATETIME, 
     PRIMARY KEY (`id`) );
 
 CREATE TABLE IF NOT EXISTS `tickets` (
     `id` INTEGER UNSIGNED NOT NULL AUTO_INCREMENT, 
-    `ticketNumber` TEXT, 
-    `ticketType` TEXT,
+    `ticketNumber` VARCHAR(25), 
+    `ticketType` VARCHAR(25),
     `createdDate` DATETIME DEFAULT CURRENT_TIMESTAMP, 
     `closedDate` DATETIME, 
-    `status` TEXT, 
-    `rate` REAL, 
-    `ticketCost` REAL, 
-    `balance` REAL,
-    `username` TEXT,
+    `status` VARCHAR(25), 
+    `rate` FLOAT, 
+    `ticketCost` FLOAT, 
+    `balance` FLOAT,
+    `username` VARCHAR(25),
     PRIMARY KEY(`id`) );
 
 CREATE TABLE IF NOT EXISTS `specialTickets` (
     `id` INTEGER UNSIGNED NOT NULL AUTO_INCREMENT, 
-    `vehicleRegistration` TEXT,
-    `ticketType` TEXT, 
+    `vehicleRegistration` VARCHAR(7),
+    `ticketType` VARCHAR(25), 
     `startDate` DATETIME, 
     `endDate` DATETIME,
     `createdDate` DATETIME DEFAULT CURRENT_TIMESTAMP, 
     `closedDate` DATETIME,  
-    `rate` REAL, 
-    `status` TEXT, 
-    `ticketCost` REAL, 
-    `balance` REAL, 
-    `noOfVisits` SMALLINT,
-    `username` TEXT,
+    `rate` FLOAT, 
+    `status` VARCHAR(25), 
+    `ticketCost` FLOAT, 
+    `balance` FLOAT, 
+    `noOfVisits` SMALLINT UNSIGNED,
+    `username` VARCHAR(25),
     PRIMARY KEY(`id`) );
 
 CREATE TABLE IF NOT EXISTS `receipts` (
     `id` INTEGER UNSIGNED NOT NULL AUTO_INCREMENT, 
-    `ticketNumber` TEXT, 
-    `ticketType` TEXT,
+    `ticketNumber` VARCHAR(25), 
+    `ticketType` VARCHAR(25),
     `createdDate` DATETIME DEFAULT CURRENT_TIMESTAMP, 
     `closedDate` DATETIME, 
-    `status` TEXT, 
-    `ticketCost` REAL, 
-    `amountPaid` REAL, 
-    `balance` REAL, 
-    `amountDue` REAL, 
-    `paymentMethod` TEXT, 
-    `chequeNumber` TEXT, 
-    `username` TEXT,
+    `status` VARCHAR(25), 
+    `ticketCost` FLOAT, 
+    `amountPaid` FLOAT, 
+    `balance` FLOAT, 
+    `amountDue` FLOAT, 
+    `paymentMethod` VARCHAR(25), 
+    `chequeNumber` VARCHAR(25), 
+    `username` VARCHAR(25),
     PRIMARY KEY(`id`) );
 
 CREATE TABLE IF NOT EXISTS `ticketType` (
     `id` INTEGER UNSIGNED NOT NULL AUTO_INCREMENT, 
-    `ticketType` TEXT, 
-    `unitCost` REAL, 
-    `status` TEXT,
+    `ticketType` VARCHAR(25), 
+    `unitCost` FLOAT, 
+    `status` VARCHAR(25),
     `createdDate` DATETIME DEFAULT CURRENT_TIMESTAMP, 
-    `username` TEXT,
+    `displayName` VARCHAR(25),
+    `description` TEXT,
+    `username` VARCHAR(25),
     PRIMARY KEY(`id`) );
 
 -- Insert default data into tables
 INSERT INTO `users` ( `username`, `password`, `firstName`, `lastName` ) VALUES ( 'admin', 'admin', 'Admin', 'User' );
-INSERT INTO `ticketType` ( `ticketType`, `unitCost`, `status`, `username` ) VALUES ('standard', 2.00, 'active', 'admin');
+INSERT INTO `ticketType` ( `ticketType`, `unitCost`, `status`, `displayName`, `description`, `username` ) VALUES ('hourly', 2.00, 'active', 'Hourly', 'Ticket for regular hourly usage.', 'admin');
